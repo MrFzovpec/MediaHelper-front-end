e690d74a208f468692398e7b0771823088bb07df
Me <keddad@yandex.ru>
2019-05-12 10:24:47 +0300

    Fixed Parsepost

--- app.py
+++ app.py
@@ -35,33 +35,39 @@ def trends(topic):
     return score
 
 
-def parsepost(post):
+def parsepost(posts):
     pic_num = 0
     doc_num = 0
     if_poll, if_longread, text_link = None, None, None
     vid_num = 0
     links = []
-    views = post['response']['items'][1]['views']['count']
-    items = post['response']['items'][1]
-    if 'attachments' in items[k]:
-        for i in range(len(items['attachments'])):
-            if items['attachments'][i]['type'] == 'photo':
-                pic_num += 1
-            if items['attachments'][i]['type'] == 'video':
-                vid_num += 1
-            if items['attachments'][i]['type'] == 'link' and 'm.vk.com/@' in items['attachments'][i]['link']['url']:
-                if_longread = True
-                text_link = items['attachments'][i]['link']['url']
-                html = requests.get(text_link).text
-                soup = BeautifulSoup(html, 'html.parser').find()
-                for k in soup.find_all('a', title=True):
-                    links.append(k['title'])
-            if items['attachments'][i]['type'] == 'poll':
-                if_poll = True
-            if items['attachments'][i]['type'] == 'doc':
-                doc_num += 1
-    return {"pic_num": pic_num, "doc_num": doc_num, "vid_num": vid_num, "if_poll": if_poll, "if_longread": if_longread,
-            "links": links, 'views': views}
+    rets = []
+    items = posts['items']
+    for k in range(len(items)):
+        if "attachments" in items[k]:
+            views = items[k]['views']['count']
+            for i in range(len(items[k]['attachments'])):
+                if items[k]['attachments'][i]['type'] == 'photo':
+                    pic_num += 1
+                if items[k]['attachments'][i]['type'] == 'video':
+                    vid_num += 1
+                if items[k]['attachments'][i]['type'] == 'link' and 'm.vk.com/@' in items[k]['attachments'][i]['link'][
+                    'url']:
+                    if_longread = True
+                    text_link = items[k]['attachments'][i]['link']['url']
+                    html = requests.get(text_link).text
+                    soup = BeautifulSoup(html, 'html.parser').find()
+                    for f in soup.find_all('a', title=True):
+                        links.append(f['title'])
+                if items[k]['attachments'][i]['type'] == 'poll':
+                    if_poll = True
+                if items[k]['attachments'][i]['type'] == 'doc':
+                    doc_num += 1
+                ret = {"pic_num": pic_num, "doc_num": doc_num, "vid_num": vid_num, "if_poll": if_poll,
+                       "if_longread": if_longread,
+                       "links": links, 'views': views}
+                rets.append(ret)
+    return rets
 
 
 def parsedoc(url):

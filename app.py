a9d49c1c9e8140936d951ff372a21b3b73ab8139
Me <keddad@yandex.ru>
2019-05-12 11:36:47 +0300

    Moved Functions

--- app.py
+++ app.py
@@ -16,26 +16,6 @@ db = SqliteDatabase('main.db')
 group_id = "-30666517"
 
 
-def trends(topic):
-    score = 0
-    time = str(datetime.datetime.now())
-    year = int(time[0:4])
-    month = int(time[5:7])
-    day = int(time[8:10])
-    hour = int(time[11:13])
-    pytrends = TrendReq(hl='ru-RU', tz=360)
-    smth = \
-        pytrends.get_historical_interest([topic], year_start=year, month_start=month, day_start=day - 7,
-                                         hour_start=hour,
-                                         year_end=year,
-                                         month_end=month, day_end=day, hour_end=hour, cat=0, geo='', gprop='', sleep=0)[
-            topic]
-    for i in range(0, 167):
-        score += smth[-i]
-    score = float(score / 168)
-    return score
-
-
 def parsepost(posts):
     pic_num = 0
     doc_num = 0
